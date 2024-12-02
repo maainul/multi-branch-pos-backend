1781528c6065607011485f55668d7e0482efecc5
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-12-02 17:45:07 +0600

    update port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pos.netlify.app"],
+    origin: ["http://localhost:5173", "https://pos.netlify.app"],
     credentials: true,
   })
 );
